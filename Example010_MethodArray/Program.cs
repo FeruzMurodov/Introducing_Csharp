eb4feea9273bcb1266619d45e52cbd66d198bb3d
Feruz <feruz.muradoff.93@mail.ru>
2023-06-12 22:56:05 +0500

    example 010 method array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 11, 82, 73, 64, 55, 46, 37, 18, 19 };
+int N = array.Length;
+int find = 18;
+int index=0;
+ while(index<N)
+ {
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        }
+    index++;
+ }
